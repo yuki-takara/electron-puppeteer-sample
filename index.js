5469f9c19b8b0c6dcd4e571230820791eb8df246
yuki-takara <tkra.yuuki@gmail.com>
2020-08-13 15:10:56 +0900

    feat: electronでgoogleの画面を表示出来るようにする

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-// Electronの読み込み
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
+const {BrowserWindow, app} = require("electron");
+const pie = require("puppeteer-in-electron")
+const puppeteer = require("puppeteer-core");
 
-// mainWindow変数の初期化
-var mainWindow = null;
+const main = async () => {
+  await pie.initialize(app);
+  const browser = await pie.connect(app, puppeteer);
 
-// MacOS(darwin)でない場合にはアプリを終了する
-app.on('window-all-closed', function() {
-  if(process.platform !== 'darwin')
-    app.quit();
-});
+  const window = new BrowserWindow();
+  const url = "https://google.com/";
+  await window.loadURL(url);
 
-// 画面を表示．index.htmlを読み込む
-// Close処理を行う
-app.on('ready', function() {
-  // 画面表示
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  const page = await pie.getPage(browser, window);
+  console.log(page.url());
+  window.destroy();
+};
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
+main();
